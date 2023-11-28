5b3f8b2783da2e8c3198ddc6b86c6d2ff45cbdf2
Paulo Roberto <paulorobertomrtjy@gmail.com>
2023-11-28 17:05:11 -0300

    Initial commit: initial structure

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,94 +1,37 @@
-import Image from 'next/image'
-import styles from './page.module.css'
+import { Header } from './components/header/header'
+import './styles/home.scss'
 
 export default function Home() {
   return (
-    <main className={styles.main}>
-      <div className={styles.description}>
+    <main className="container">
+      <Header />
+      {/* EXPERIENCIA */}
+      <div className="experience">
+        <h3>Experience</h3>
         <p>
-          Get started by editing&nbsp;
-          <code className={styles.code}>src/app/page.tsx</code>
+          Lorem ipsum dolor sit amet consectetur, adipisicing elit. Architecto
+          ducimus inventore.
         </p>
-        <div>
-          <a
-            href="https://vercel.com?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            By{' '}
-            <Image
-              src="/vercel.svg"
-              alt="Vercel Logo"
-              className={styles.vercelLogo}
-              width={100}
-              height={24}
-              priority
-            />
-          </a>
+        <div className="experience-time"></div>
+
+        <div className="infos">
+          <h3>Languages</h3>
+          <div className="languages-info">
+            <span>EN - Fluent</span>
+            <span>PT-BR - Native Speaker</span>
+          </div>
+
+          <h3>Education</h3>
+          <div className="educational-info">
+            <span>Icon</span>
+            <span>Texto sobre onde eu me formei.</span>
+          </div>
         </div>
-      </div>
 
-      <div className={styles.center}>
-        <Image
-          className={styles.logo}
-          src="/next.svg"
-          alt="Next.js Logo"
-          width={180}
-          height={37}
-          priority
-        />
-      </div>
-
-      <div className={styles.grid}>
-        <a
-          href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Docs <span>-&gt;</span>
-          </h2>
-          <p>Find in-depth information about Next.js features and API.</p>
-        </a>
-
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Learn <span>-&gt;</span>
-          </h2>
-          <p>Learn about Next.js in an interactive course with&nbsp;quizzes!</p>
-        </a>
-
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Templates <span>-&gt;</span>
-          </h2>
-          <p>Explore starter templates for Next.js.</p>
-        </a>
-
-        <a
-          href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Deploy <span>-&gt;</span>
-          </h2>
-          <p>
-            Instantly deploy your Next.js site to a shareable URL with Vercel.
-          </p>
-        </a>
+        <div className="buttons">
+          <div className="social"></div>
+          <button>Contact me</button>
+        </div>
       </div>
     </main>
   )
